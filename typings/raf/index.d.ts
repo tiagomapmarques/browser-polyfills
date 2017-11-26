9c95b5dd99d12a0e7f02a7deef1c51cfae69db42
Tiago Marques <tiago.map.marques@gmail.com>
2017-11-26 15:52:48 +0000

    Added caf polyfill

--- typings/raf/index.d.ts
+++ typings/raf/index.d.ts
@@ -1,6 +1,12 @@
 
 declare module 'raf' {
+  type cafType = (handle: number) => void;
   type rafType = (callback: FrameRequestCallback) => number;
-  let requestAnimationFrame: rafType;
+
+  interface Raf extends rafType {
+    cancel: cafType;
+  }
+
+  let requestAnimationFrame: Raf;
   export = requestAnimationFrame;
 }
